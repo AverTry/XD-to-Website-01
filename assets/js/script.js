af56e4c90c57ee9f82471d3a0ca3784b99745b42
AverTry <72357571+AverTry@users.noreply.github.com>
2022-03-03 11:36:32 +0000

    Update script.js
    Click logo instead of nav

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,17 @@
 const navPos = getComputedStyle(document.documentElement).getPropertyValue('--navPos')
 const navbar = document.querySelector('nav')
+const logo = document.querySelector('.logo')
 const ocean = document.querySelector('.ocean')
 const animLink = document.querySelector('.journeyAnimContainer')
 const journeyStarting = document.querySelector('#journeyStarting')
-window.onscroll = () => { 
-  window.scrollY > navPos 
-  ? navbar.classList.add('navShrink') 
-  : navbar.classList.remove('navShrink') 
+window.onscroll = () => {
+	window.scrollY > navPos ? navbar.classList.add('navShrink') : navbar.classList.remove('navShrink')
 }
-navbar.onclick = () => navbar.classList.toggle('mobile')
-animLink.onclick = () => {
-  journeyStarting.innerText = 'Starting Your Journey...'
-  ocean.classList.add('runAnim')
-  setTimeout(() => location ='./journey.html', 5000)
+logo.onclick = () => navbar.classList.toggle('mobile')
+window.onload = () => {
+	animLink.onclick = () => {
+		journeyStarting.innerText = 'Starting Your Journey...'
+		ocean.classList.add('runAnim')
+		setTimeout(() => (location = './journey.html'), 5000)
+	}
 }
